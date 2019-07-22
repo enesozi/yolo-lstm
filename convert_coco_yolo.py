b7a8c6c937651e266df1070add9d320a31041ecf
enesozi <enesoziepk05@gmail.com>
2019-07-22 12:34:45 +0000

    dataset prefix option added

--- convert_coco_yolo.py
+++ convert_coco_yolo.py
@@ -11,16 +11,19 @@ if __name__ == '__main__':
     parser.add_argument(
         "path", help='Json file containing annotations')
     parser.add_argument(
+        "dataset", help='Json file containing annotations')
+    parser.add_argument(
         "output_path", help='Output directory for image.txt files')
     args = parser.parse_args()
 
-    with open(json_file) as f:
+    with open(args.path) as f:
         data = json.load(f)
         annotations = data['annotations']
         width = 1920
         height = 1208
         converted_results = []
-        for ann in annotations:
+        image_ids = len(data['images']) - 1
+        for ann in []:
             cat_id = int(ann['category_id'])
             left, top, bbox_width, bbox_height = map(
                 float, ann['bbox'])
@@ -34,6 +37,10 @@ if __name__ == '__main__':
             w_rel, h_rel = (bbox_width / width, bbox_height / height)
             converted_results.append(
                 (cat_id, x_rel, y_rel, w_rel, h_rel))
-        with open(os.path.join(args.output_path, ann['image_id'].split('.')[0] + '.txt'), 'w+') as fp:
-            fp.write('\n'.join('%d %.6f %.6f %.6f %.6f' %
-                               res for res in converted_results))
+            file_name = "%s_%s.txt" % (
+                args.dataset, ann['image_id'].split('.')[0])
+            with open(os.path.join(args.output_path, file_name), 'w+') as fp:
+                fp.write('\n'.join('%d %.6f %.6f %.6f %.6f' %
+                                   res for res in converted_results))
+    with open(args.dataset+'.txt','w+') as f:
+       f.write('%d'%image_ids)
